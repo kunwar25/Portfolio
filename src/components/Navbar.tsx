ad7d9d02927268a0702d54029f977d5cdcaac4c9
kunwar25 <pk304115@gmail.com>
2025-02-02 17:52:00 +0530

    final changes for mobile screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar() {
     return (
       <nav className="fixed top-0 left-0 w-full bg-gray-900 text-white shadow-lg z-50">
         <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <div className="text-xl font-bold tracking-wide text-blue-400">
+        <div className="text-lg font-bold tracking-wide text-blue-400">
             PRIYANSHU
           </div>
           {/* Left Side - Navigation Links */}
-          <ul className="flex space-x-6 text-lg font-medium">
+          <ul className="flex space-x-3 text-lg font-medium">
             <li><Link href="/" className="hover:text-blue-400 transition">Home</Link></li>
            
             <li><Link href="projects" className="hover:text-blue-400 transition">Projects</Link></li>
