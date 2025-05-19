7bf1502b7513e38eab1c7c62ad096edeecec2c82
kunwar25 <pk304115@gmail.com>
2025-05-20 01:31:05 +0530

    Updating the resume

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -87,7 +87,7 @@ function HeroSection() {
 
         </div>
         <div className="mt-4">
-  <Link href="/Priyanshu_Kumar_resume.pdf" download>
+  <Link href="/Priyanshu_resume.pdf" download>
     <button className="p-[3px] relative">
       <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
       <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-center">
